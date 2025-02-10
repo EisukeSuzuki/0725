930712eb76086469504b85c78021ed40e17d4049
EisukeSuzuki <162996717+EisukeSuzuki@users.noreply.github.com>
2025-02-10 12:04:07 +0900

    Add files via upload
    Signed-off-by: EisukeSuzuki <162996717+EisukeSuzuki@users.noreply.github.com>

---
+++ Webhook.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+webhook_url = "https://prod-73.westus.logic.azure.com:443/workflows/1b12f9eab547427cba9ce839aedc6f9d/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=-CtSG3GpaLPzTw5jr3wJqVIAVe1gnDaydjMci-Kr8NA"
+message = {
+    "type": "message",
+    "attachments": [
+        {
+            "contentType": "application/vnd.microsoft.card.adaptive",
+            "content": {
+                "type": "AdaptiveCard",
+                "body": [
+                    {
+                        "type": "TextBlock",
+                        "text": "メッセージテスト"
+                    }
+                ]
+            }
+        }
+    ]
+}
+
+response = requests.post(webhook_url, headers={"Content-Type": "application/json"}, json=message)
+print(response.status_code)
